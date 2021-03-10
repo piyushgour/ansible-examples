b3bf86ac1119365bb199d540b5ada0abd71429a0
root <root@IMPETUS-IL0028.impetus.co.in>
2021-03-10 12:47:18 +0530

    Bash script add

---
+++ ansible-molecule-test.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+current_dir=`pwd`
+echo "$current_dir"
+roles="role"
+role_list=`ls $roles/`
+echo "$role_list"
+for role_dir in $role_list
+    do
+	cd $roles/$role_dir
+    molecule test
+	if [ "$?" != "0" ]; then
+	break
+	fi
+done
